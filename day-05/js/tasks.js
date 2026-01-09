39be6ee368e4ec9ca8d0a54c381a4c13ce5c0065
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-01-09 17:59:42 +0100

    feat: Done all thoses tasks

--- day-05/js/tasks.js
+++ day-05/js/tasks.js
@@ -14,13 +14,28 @@ for (l = 1, m = 10; l <= 10, m >= 1; l++, m--) {
  *  ******
  */
 function RepeatStar(start) {
-  let acumolo = "";
   for (linha = 1; linha <= start; linha++) {
+    let acumolo = "";
     for (col = 1; col <= linha; col++) {
       acumolo += "*";
     }
-    console.log(acumolo);
+    // console.log(acumolo);
   }
 }
 
 RepeatStar(2);
+
+/**
+ * Encher copos
+ * Copo 1-> 1 dose;
+ * Copo 2-> 2 doses;
+ * Copo 3-> 3 doses;
+ */
+
+function FillUp(n) {
+  for (w = 1; w <= n; w++) {
+    console.log(`Copo ${w} -> ${w} ${!(w === 1) ? "doses" : `dose`} `);
+  }
+}
+
+FillUp(4);
