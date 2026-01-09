b21d6cde6bb6058a5e4e21c562967f11df701eaf
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-01-09 12:18:02 +0100

    feat: task 5 didn't finished yet

--- day-05/js/tasks.js
+++ day-05/js/tasks.js
@@ -1,5 +1,26 @@
 //Task aula
 
 for (l = 1, m = 10; l <= 10, m >= 1; l++, m--) {
-  console.log(`L = ${l}, M = ${m}`);
+  // console.log(`L = ${l}, M = ${m}`);
 }
+
+/**
+ * Tasks
+ *  *
+ *  **
+ *  ***
+ *  ****
+ *  *****
+ *  ******
+ */
+function RepeatStar(start) {
+  let acumolo = "";
+  for (linha = 1; linha <= start; linha++) {
+    for (col = 1; col <= linha; col++) {
+      acumolo += "*";
+    }
+    console.log(acumolo);
+  }
+}
+
+RepeatStar(2);
