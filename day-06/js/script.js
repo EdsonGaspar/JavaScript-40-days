784b836208f9448279d0a6a16f1384b68fc58029
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-01-12 08:34:47 +0100

    feat: Tasks didn't finished yet

--- day-06/js/script.js
+++ day-06/js/script.js
@@ -70,4 +70,83 @@ const error = function (messageErro) {
   console.log(`${messageErro}!!!`);
 };
 
-ValidateAcess("Hadson", 21, sucess, error);
+// ValidateAcess("Hadson", 21, sucess, error);
+
+//Função Pura, quer dizer que está funcão terá os mesmos outputs para os mesmos inputs
+let message = "Hello";
+function Greetings(name) {
+  console.log(`${message}, ${name}`);
+}
+
+Greetings("TypaScripts");
+message = "Namaste";
+Greetings("TypaScripts");
+Greetings("TypaScripts");
+Greetings("TypaScripts");
+
+//Heigher Older function
+
+function ReturnFunc() {
+  return function Say() {
+    console.log("Hello");
+  };
+}
+
+const retFunc = ReturnFunc();
+
+// retFunc();
+
+/**
+ * Arrow Function
+ */
+
+let getGreetMe = (smsLanguage) => {
+  console.log(`Welcome to study arrow function in ${smsLanguage}`);
+};
+
+// getGreetMe("PHP");
+
+/**
+ * IIFE(Immediately Invoked Function Expression), a função é executada imdediatamente depois da sua declaração;
+ */
+
+(function (rank) {
+  console.log("IIFE, function immdiately invoked", rank);
+})(91);
+
+/**
+ * Function Execution Stack(Call Stack), empilha o primeiro a entrar é o último a sair
+ * Como se cocolase-mos um para um em cima do outro e so podendo tirar o ultimo colocado.
+ */
+
+function A() {
+  console.log("A, inicio");
+  B();
+  console.log("A, terminou");
+}
+
+function B() {
+  console.log("B, inicio");
+  C();
+  console.log("B, terminou");
+}
+
+function C() {
+  console.log("C, executou");
+}
+
+A();
+
+/**
+ * Recursion Function, e quando uma funcao chama a si mesma
+ */
+function GetWater(count) {
+  console.log(`Pegando agua ${count}`);
+  if (count === 0) {
+    console.log("Terminou de pegar agua.");
+    return;
+  }
+  GetWater((count -= 1));
+}
+
+GetWater(5);
