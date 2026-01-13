b6b80389142f4d9bfd1fc43cef744996d84bb9f1
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-01-13 15:50:04 +0100

    feat: tasks pendentes da aula 07

--- day-07/js/script.js
+++ day-07/js/script.js
@@ -1,52 +1,17 @@
 /**
- * Jogo de pedra papel e tesoura
- * Pedra quebra Tesoura → Pedra vence
- * Papel embrulha Pedra → Papel vence
- * Tesoura corta Papel → Tesoura vence
+ * Programa para adevinhar um numero entre 0 a 10
+ * max number
+ * min number
+ *
+ * secret number is a random number between the min and max
+ *
+ * get the prompt from the user as guess
+ *
+ * create a variable to store the number of attemps
+ *
+ * write the logic to check if the guess and the secret number are same. If not continue a loop to ask again
+ *
+ * Inside loop check if the guess is less than the secret or greater than, accorddingly tell if the guess it low or high.
+ *
+ * Give the option to play agian.
  */
-
-function ChooseHand() {
-  console.log("Comecou o Jogo!");
-  const userChoicePrompt = prompt("Entra com Papel, Pedra ou Tesoura");
-  const userChoice = userChoicePrompt.toLocaleLowerCase();
-
-  let computerChoice;
-  const randomComputer = Math.floor(Math.random() * 3) + 1;
-  console.log(randomComputer);
-
-  if (randomComputer === 1) {
-    computerChoice = "pedra";
-  } else if (randomComputer === 2) {
-    computerChoice = "papel";
-  } else if (randomComputer === 3) {
-    computerChoice = "tesoura";
-  }
-
-  console.log("Escolha do usuario: ", userChoice);
-  console.log("Escolha do computador: ", computerChoice);
-
-  if (
-    (userChoice === "pedra" && computerChoice === "tesoura") ||
-    (userChoice === "papel" && computerChoice === "pedra") ||
-    (userChoice === "tesoura" && computerChoice === "papel")
-  ) {
-    console.log("Yay, você é o vencedor!");
-  } else if (userChoice === computerChoice) {
-    console.log("Empate!!!");
-    /**
-     * c- tesoura corta papel → tesoura vence
-     * c- papel embrulha Pedra → pedra vence
-     * c- pedra corta tesoura → Tesoura vence
-     */
-  } else if (
-    (userChoice === "papel" && computerChoice === "tesoura") ||
-    (userChoice === "pedra" && computerChoice === "papel") ||
-    (userChoice === "tesoura" && computerChoice === "pedra")
-  ) {
-    console.log("Ohh, computador venceu!!!");
-  } else {
-    console.log("Situação não esperada!");
-  }
-}
-
-ChooseHand();
