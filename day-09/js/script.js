8410cd695eca20251d9ad9a289e6d94420fb1d00
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-01-15 23:24:02 +0100

    feat hosting

---
+++ day-09/js/script.js
@@ -0,0 +1,13 @@
+//Hosting
+console.log("Nome", nome);
+var nome;
+nome = "Edson";
+console.log("Nome: ", nome);
+
+//Temporal dead zone, é a areao onde não se pode acessar uma variavel até a sua inicialização.
+
+var test = function () {
+  console.log("Meu mundo esta on line");
+};
+
+test();
