ed6210c8371a1381be0d2838a22e1b01e70da7bf
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-04 17:17:48 +0100

    feat: Created Object, function construtor...

--- day-12-again/js/script01.js
+++ day-12-again/js/script01.js
@@ -16,9 +16,28 @@ console.log(`Nome: ${nome} Genero: ${genero}`);
 
 user.isCitezen = false;
 
-console.log(user);
+// console.log(user);
 
 //Propriedade com caracteres especiais
 user["is employ"] = true;
 
-console.log(user["is employ"]);
+// console.log(user["is employ"]);
+
+/**
+ * Propriedade dinamica
+ */
+user.age = 23;
+
+let someKey = "age";
+
+console.log(user[someKey]);
+
+//Escreva um algoritmo que o usuario escolhe o tipo de carro que gosta e retribuio o tipo de carro com o numero 5
+
+const car = prompt("Qual e o seu carro favorito?");
+
+const cars = {
+  [car]: "5",
+};
+
+console.log(`${car} ${cars[car]}`);
