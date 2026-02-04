2b643692601fc960cf899912644598d50a90042c
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-04 14:58:06 +0100

    bug: I'm not get

---
+++ day-06/js/task05.js
@@ -0,0 +1,11 @@
+/**
+ * Crie uma funcao que conte as vogais em uma frase
+ */
+
+function countVaawels(frase) {
+  for (let i = 0; i < frase.lenght; i++) {
+    console.log(frase);
+  }
+}
+
+countVaawels("Ana");
