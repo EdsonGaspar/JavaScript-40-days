d299e01d309b1be6fd01550c937c28cbd690322d
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-06 15:24:45 +0100

    feat: Ainda estudando metodos estaticos, haswon

--- day-12-again/js/script05.js
+++ day-12-again/js/script05.js
@@ -19,3 +19,41 @@ const entreis = new Map([
 
 const becomeObject = Object.fromEntries(entreis);
 console.log(becomeObject);
+
+//O que e imutabilidade em objectos. O objecto fica completamente imutavel
+/**
+ * Congelar o objecto para que nao se faca alteracoes nele
+ */
+
+const emp = {
+  sal: 100,
+};
+
+Object.freeze(emp);
+
+emp.custo = 20;
+delete emp.sal;
+console.log(emp); //Continua a ser o objecto inincial
+
+//Verificar se um determinado objecto esta congelado ou nao "freeze"
+console.log(Object.isFrozen(emp)); //true
+
+//Diferenca entre F reeze e Seal
+
+const depart = {
+  nome: "finance",
+};
+
+Object.seal(depart);
+
+depart.adress = "Luanda"; //Acao  nao permitida
+delete depart.nome; //Acao nao permitida
+
+depart.nome = "RH"; //Acao permitida
+
+console.log(depart);
+
+//HasOwn, metodo estatico que permite verificar se em um determindado objecto tem uma chave x. Retornando true ou false.
+
+const isExist = Object.hasOwn(depart, "address");
+console.log("Existe", isExist);
