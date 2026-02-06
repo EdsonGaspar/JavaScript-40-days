871207f04230ad458fe14d8d7581d30cea1f1b7e
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-06 16:21:35 +0100

    feat: Destruction in loop

--- day-12-again/js/script07.js
+++ day-12-again/js/script07.js
@@ -27,3 +27,15 @@ const age = anotherStudent.age;
 const { name: anotherName, age: anotherAge } = getStudent();
 console.log(`Sem desestruturacao Nome: ${name} Idade: ${age}`);
 console.log(`Com desestruturacao Nome: ${anotherName} Idade: ${anotherAge}`);
+
+//Destructin in loop
+
+const students = [
+  { name: " Willian", grade: "A+" },
+  { name: " Tom", grade: "B+" },
+  { name: " Paul", grade: "A" },
+];
+
+for (let { name, grade } of students) {
+  console.log(`Name: ${name} Idade: ${grade}`);
+}
