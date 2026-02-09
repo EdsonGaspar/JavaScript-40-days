ec2e2b0f0da922a3869ced1da7839921e802a159
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-09 12:44:45 +0100

    fix: Task 07 done

--- day-13/js/task05.js
+++ day-13/js/task05.js
@@ -12,5 +12,36 @@ class Sports {
 const futbool = new Sports("Recriativo da Caala", 21);
 const basketbool = new Sports("1 da Gosto", 18);
 
-futbool.log();
-basketbool.log();
+// futbool.log();
+// basketbool.log();
+
+//Task 06
+const car1 = {
+  brand: "Audi",
+  model: "A8",
+  describe: function () {
+    console.log(`This car is a ${this.brand} ${this.model}.`);
+  },
+};
+
+const car2 = {
+  brand: "BMW",
+  model: "X1",
+}; //Nao resolvido.
+
+//Task 07
+const person = {
+  name: "Charlie",
+  sayHello: function () {
+    console.log(this.name);
+  },
+  sayHelloArrow() {
+    const inner = () => {
+      console.log(this.name);
+    };
+    inner();
+  },
+};
+
+person.sayHello();
+person.sayHelloArrow();
