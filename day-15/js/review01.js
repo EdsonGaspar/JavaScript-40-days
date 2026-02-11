794a868ecaf43be3a88498c1e83ebc447924b53d
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-11 15:09:35 +0100

    feat: Adicionando um exemplo real.

--- day-15/js/review01.js
+++ day-15/js/review01.js
@@ -15,3 +15,37 @@ const firstElementRemoved = salads.shift(); //Remove o primeiro elemento da list
 console.log(firstElementRemoved);
 
 console.log(salads);
+
+//EXEMPLO DA VIDA REAL
+//Faca um sistema que permite enviar email
+class SistemOfNotification {
+  constructor() {
+    this.notificoes = [];
+  }
+
+  //Adiciona no inicio da lista.
+  addNotification(message) {
+    const notificao = {
+      id: Date.now(),
+      message,
+      lida: false,
+      timestamp: new Date(),
+    };
+    this.notificoes.unshift(notificao);
+  }
+
+  //Remove de maneira FIFO
+  removeNotification() {
+    this.notificoes.shift();
+  }
+  removeLast() {
+    this.notificoes.pop();
+  }
+}
+
+const sendNotification01 = new SistemOfNotification();
+sendNotification01.addNotification("Terminar tarefa de programacao");
+sendNotification01.addNotification("Ler livro a fe explicada");
+
+sendNotification01.removeLast();
+console.log("\n ", sendNotification01.notificoes);
