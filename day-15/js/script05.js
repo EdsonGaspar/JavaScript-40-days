b11995071edafd85274d3eb0e974713b04520270
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-12 08:25:12 +0100

    feat: Imutabilite and mutabilite

---
+++ day-15/js/script05.js
@@ -0,0 +1,11 @@
+//Tecnicas de imutabilidade
+//spread operator
+const fruts = ["Maça", "Banana"];
+
+//Adicionar no final, criando um novo array
+const newFruts = [...fruts, "Laranja"];
+
+//Adicionar no inicio, cria um novo array
+const newFruts2 = ["Manga", ...fruts];
+
+//Adicionar no meio
