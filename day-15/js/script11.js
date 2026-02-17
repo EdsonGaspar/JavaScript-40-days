f0a28c0fb3e680e05a983e6b935cd940a5bc4283
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-02-17 09:51:58 +0100

    feat: from , fromAsync and of

--- day-15/js/script11.js
+++ day-15/js/script11.js
@@ -21,3 +21,31 @@ console.log("Array Like ", document.getElementsByTagName("li"));
 const arrayLike = document.getElementsByTagName("li");
 const arrayTrue = Array.from(arrayLike);
 console.log("True Array", arrayTrue);
+
+//fromAsync(), retorna uma promessa
+
+const arrayLike1 = document.getElementsByTagName("li");
+const convertArrayAsync = Array.fromAsync(arrayLike1);
+
+//Apresentar dados de uma promessa
+console.log(convertArrayAsync); //Retorna uma promessa
+convertArrayAsync.then((value) => console.log(value));
+
+//Outro exemplo
+
+const ret = Array.fromAsync({
+  0: "TapasScript",
+  1: "google",
+  2: "Youtube",
+  3: "whastapp",
+  length: 4,
+});
+
+ret.then((element) => console.log(element));
+
+//of(), cria um novo array com os elementos inseridos nela
+const a = new Array(2, 3, 4);
+const b = new Array(4, 5, 6);
+
+const c = Array.of(2, 3, true, { name: "Paulo" }, [11, 12, 13]);
+console.log(c);
