d97dc79bd34ae739cca70b1ba26ed7f5cfa3035b
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-03-10 14:44:37 +0100

    feat: Creado elemento na arvore dom e manipulando

--- day-18/js/script01.js
+++ day-18/js/script01.js
@@ -1,10 +1,17 @@
-//Criando elemento
-let pElement = document.createElement("p");
-pElement.innerText = "Adicionando um texto apartir do javascript";
-document.body.appendChild(pElement);
+//Criando elemento no documento html apartie da DOM
 
-//Inserindo elemento
-const createSpan = document.createElement("span");
-createSpan.innerText = "Eu sou Span";
-let pElementIns = document.querySelector("p");
-document.body.insertBefore(createSpan, pElementIns);
+let elementoP = document.createElement("p");
+elementoP.innerText = "Este e um texto adcionado dinamicamente.";
+//Adicionando no documeto DOM
+document.body.appendChild(elementoP);
+console.log(elementoP);
+
+//Inserir um elemento antes de um elemento especifico
+const elementoSpan = document.createElement("span");
+elementoSpan.innerText =
+  "Sou elemento SPAN e serei imprimido antes do elemento P";
+
+//Selecionando ou pegando o elemento p
+const selectP = document.querySelector("p");
+
+document.body.insertBefore(elementoSpan, selectP);
