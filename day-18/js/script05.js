a8af4e41371d5318f764da652be7ebd424b18500
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-03-11 12:41:12 +0100

    feat: Manipulando atributo...

--- day-18/js/script05.js
+++ day-18/js/script05.js
@@ -3,3 +3,18 @@
 let divSelected = document.getElementById("permited");
 
 document.body.removeChild(divSelected);
+
+{
+  //Read, write and remove atribute
+  let getImage = document.querySelector("img");
+
+  //Escrevendo atributo
+  getImage.setAttribute("src", "./assets/transferir.jpg");
+  getImage.setAttribute("alt", "banner");
+
+  //Removendo atributo
+  getImage.removeAttribute("width");
+
+  //Lendo atributo
+  console.log(getImage.getAttribute("src"));
+}
